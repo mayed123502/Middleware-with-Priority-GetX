ee80aab733abb8e177bfe7101441a752df327216
mayed123502 <moayedalharazeen@gmail.com>
2022-06-20 18:13:56 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:learn_getx/views/MyHomePage.dart';
+import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:learn_getx/views/home.dart';
+import 'package:learn_getx/views/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +13,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: Home(),
+      getPages: [
+        GetPage(name: "/", page: () => const Login()),
+        GetPage(name: "/Home", page: () => Home()),
+      ],
     );
   }
 }
---
+++ lib/views/home.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/mainControler.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Screen")),
+      body: null,
+    );
+  }
+}
